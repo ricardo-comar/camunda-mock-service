dad1204c41a08ac0f6644a8c6193dcc3ae2cdbd1
Ricardo Henrique Comar <ricardo.comar@gmail.com>
2020-09-28 19:53:55 -0300

    failure validations

--- src/main/java/com/github/ricardocomar/camunda/mockservice/validator/FailureValidator.java
+++ src/main/java/com/github/ricardocomar/camunda/mockservice/validator/FailureValidator.java
@@ -2,20 +2,31 @@ package com.github.ricardocomar.camunda.mockservice.validator;
 
 import static br.com.fluentvalidator.predicate.LogicalPredicate.not;
 import static br.com.fluentvalidator.predicate.StringPredicate.stringEmptyOrNull;
-import com.github.ricardocomar.camunda.mockservice.model.Condition;
+import static br.com.fluentvalidator.predicate.ComparablePredicate.greaterThan;
+import static br.com.fluentvalidator.predicate.ObjectPredicate.nullValue;
+import com.github.ricardocomar.camunda.mockservice.model.Failure;
 import org.springframework.stereotype.Component;
 import br.com.fluentvalidator.AbstractValidator;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Component
-public class ConditionValidator extends AbstractValidator<Condition> {
+public class FailureValidator extends AbstractValidator<Failure> {
 
     @Override
     public void rules() {
 
-        ruleFor("conditionScript", Condition::getConditionScript).must(not(stringEmptyOrNull()))
-                .withMessage("conditionScript is mandatory").critical();
+        ruleFor("details", Failure::getDetails).must(not(stringEmptyOrNull()))
+                .withMessage("Details is mandatory").critical();
+
+        ruleFor("message", Failure::getMessage).must(not(stringEmptyOrNull()))
+                .withMessage("Message is mandatory").critical();
+
+        ruleFor("retryTimes", Failure::getRetryTimes).must(greaterThan(0)).when(not(nullValue()))
+                .withMessage("Message is mandatory").critical();
+
+        ruleFor("retryTimeout", Failure::getRetryTimeout).must(greaterThan(0L)).when(not(nullValue()))
+                .withMessage("Message is mandatory").critical();
 
     }
 
