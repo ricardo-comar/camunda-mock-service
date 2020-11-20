35e02c6460346e1efc4a4752cdb0e7973c19cdc8
Ricardo Henrique Comar <ricardo.comar@gmail.com>
2020-11-20 19:02:33 -0300

    final changes

--- maven-plugin/src/main/java/com/github/ricardocomar/camunda/mockservice/plugin/MockMojo.java
+++ maven-plugin/src/main/java/com/github/ricardocomar/camunda/mockservice/plugin/MockMojo.java
@@ -11,30 +11,24 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import org.springframework.boot.SpringApplication;
 
-@Mojo(name = "camunda-mock-service", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST,
+@Mojo(name = "camunda-mock-service", defaultPhase = LifecyclePhase.PROCESS_TEST_CLASSES,
         requiresDependencyResolution = ResolutionScope.RUNTIME)
 public class MockMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     MavenProject project;
 
-    private static final Thread thread = new Thread(() -> {
-        SpringApplication.run(PluginApplication.class, new String[] {});
-    });
+    @Parameter(property = "scenariosFolder",
+            defaultValue = "${project.build.testOutputDirectory}/scenarios")
+    private String scenariosFolder;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
 
-
         getLog().info("Starting Spring Context");
-        thread.start();
-
-        getLog().info("Sleeping 15s...");
-        try {
-            Thread.sleep(15000);
-        } catch (final Exception e) {
-        }
-        getLog().info("Awake !");
+        SpringApplication.run(PluginApplication.class,
+                new String[] {"--scenariosFolder=" + scenariosFolder});
+        getLog().info("started!");
 
     }
 
